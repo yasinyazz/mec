4e21c3316717fd6f6ca160f13fd08e040a526923
yasinyazz <60429168+yasinyazz@users.noreply.github.com>
2023-11-23 06:43:08 +0530

    updated
    Polynomial addition using linked list

--- polyLL.c
+++ polyLL.c
@@ -1,93 +1,125 @@
 #include<stdio.h>
 #include<stdlib.h>
 
-typedef struct node{
-    int coef;
-    int exp;
-    struct node *next;
-}Polynomial;
+// Node definition
+typedef struct Node {
+   int coef;
+   int exp;
+   struct Node* next;
+} Node;
 
-void addTerm(Polynomial **poly, int coef, int exp){
-    Polynomial *newNode = (Polynomial*)malloc(sizeof(Polynomial));
-    newNode->coef = coef;
-    newNode->exp = exp;
-    newNode->next = NULL;
+// Function prototypes
+void addTerms(Node**, int, int);
+void addPolynomials(Node* first, Node* second);
+void printPolynomial(Node*);
+void deletePolynomial(Node**);
 
-    if(*poly == NULL){
-        *poly = newNode;
-    } else {
-        Polynomial *last = *poly;
-        while(last->next != NULL){
-            last = last->next;
-        }
-        last->next = newNode;
-    }
+// Main function
+int main() {
+   int val, choice, coef, exp;
+   Node *first = NULL, *second = NULL;
+
+   printf("Polynomial Operations: Addition and Display\n");
+   do {
+       printf("\nChoose operation:\n1. Add term to polynomial 1\n2. Add term to polynomial 2\n3. Add polynomials\n4. Display polynomials\n5. Exit\n");
+       scanf("%d", &choice);
+
+       switch (choice) {
+           case 1:
+               printf("\nEnter coefficient and exponent: ");
+               scanf("%d %d", &coef, &exp);
+               addTerms(&first, coef, exp);
+               break;
+
+           case 2:
+               printf("\nEnter coefficient and exponent: ");
+               scanf("%d %d", &coef, &exp);
+               addTerms(&second, coef, exp);
+               break;
+
+           case 3:
+               addPolynomials(first, second);
+               break;
+
+           case 4:
+               printf("\nPolynomial 1:\n");
+               printPolynomial(first);
+               printf("\nPolynomial 2:\n");
+               printPolynomial(second);
+               break;
+
+           case 5:
+               deletePolynomial(&first);
+               deletePolynomial(&second);
+               break;
+       }
+   } while (choice != 5);
+
+   return 0;
 }
 
-Polynomial* addPolynomials(Polynomial *poly1, Polynomial *poly2){
-    Polynomial *result = NULL;
-    Polynomial *ptr1 = poly1;
-    Polynomial *ptr2 = poly2;
-
-    while(ptr1 != NULL && ptr2 != NULL){
-        if(ptr1->exp > ptr2->exp){
-            addTerm(&result, ptr1->coef, ptr1->exp);
-            ptr1 = ptr1->next;
-        } else if(ptr1->exp < ptr2->exp){
-            addTerm(&result, ptr2->coef, ptr2->exp);
-            ptr2 = ptr2->next;
-        } else {
-            int sum = ptr1->coef + ptr2->coef;
-            if(sum != 0){
-                addTerm(&result, sum, ptr1->exp);
-            }
-            ptr1 = ptr1->next;
-            ptr2 = ptr2->next;
-        }
-    }
-
-    while(ptr1 != NULL){
-        addTerm(&result, ptr1->coef, ptr1->exp);
-        ptr1 = ptr1->next;
-    }
-
-    while(ptr2 != NULL){
-        addTerm(&result, ptr2->coef, ptr2->exp);
-        ptr2 = ptr2->next;
-    }
-
-    return result;
+// Function to add a new term to the polynomial
+void addTerms(Node** poly_ref, int coef, int exp) {
+   Node* new_node = (Node*) malloc(sizeof(Node));
+   new_node->coef = coef;
+   new_node->exp = exp;
+   new_node->next = *poly_ref;
+   *poly_ref = new_node;
 }
 
-void printPolynomial(Polynomial *poly){
-    Polynomial *ptr = poly;
-    while(ptr != NULL){
-        printf("%dx^%d", ptr->coef, ptr->exp);
-        ptr = ptr->next;
-        if(ptr != NULL){
-            printf(" + ");
-        }
-    }
-    printf("\n");
+// Function to add two polynomials
+void addPolynomials(Node* first, Node* second) {
+   Node* new_poly = NULL;
+
+   while (first != NULL && second != NULL) {
+       if (first->exp > second->exp) {
+           addTerms(&new_poly, first->coef, first->exp);
+           first = first->next;
+       } else if (first->exp < second->exp) {
+           addTerms(&new_poly, second->coef, second->exp);
+           second = second->next;
+       } else {
+           int coef_sum = first->coef + second->coef;
+           if (coef_sum != 0)
+               addTerms(&new_poly, coef_sum, first->exp);
+           first = first->next;
+           second = second->next;
+       }
+   }
+
+   while (first != NULL) {
+       addTerms(&new_poly, first->coef, first->exp);
+       first = first->next;
+   }
+
+   while (second != NULL) {
+       addTerms(&new_poly, second->coef, second->exp);
+       second = second->next;
+   }
+
+   printPolynomial(new_poly);
+   deletePolynomial(&new_poly);
 }
 
-int main(){
-    Polynomial *poly1 = NULL;
-    Polynomial *poly2 = NULL;
+// Function to print the polynomial
+void printPolynomial(Node* node) {
+   while (node != NULL) {
+       printf("%dx^%d ", node->coef, node->exp);
+       node = node->next;
+   }
+   printf("\n");
+}
 
-    addTerm(&poly1, 2, 3);
-    addTerm(&poly1, 3, 2);
-    addTerm(&poly1, 1, 0);
+// Function to delete the polynomial
+void deletePolynomial(Node** poly_ref) {
+   Node* current = *poly_ref;
+   Node* next;
 
-    addTerm(&poly2, 1, 3);
-    addTerm(&poly2, 4, 1);
-    addTerm(&poly2, 5, 0);
+   while (current != NULL) {
+       next = current->next;
+       free(current);
+       current = next;
+   }
 
-    printPolynomial(poly1);
-    printPolynomial(poly2);
-
-    Polynomial *result = addPolynomials(poly1, poly2);
-    printPolynomial(result);
-
-    return 0;
+   *poly_ref = NULL;
 }
